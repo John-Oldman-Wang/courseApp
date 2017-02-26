a45206b295b5bedb3597faa91e94d4272e01eeb8
wanglin <mag1c_shen_09@foxmail.com>
2017-02-26 09:19:34 +0800

    debug upload
    debug upload

--- app.js
+++ app.js
@@ -26,24 +26,7 @@ app.use(session({
 }))
 require("./config/Middleware")(app)
 
-app.use(function(req,res,next){
-	if(req.files){
-		console.log("youwenjian")
-		var files=req.files
-		var oldname=[]
-		var type=[]
-		oldname.push(files["avatar"][0].filename)
-		oldname.push(files["gallery"][0].filename)
-		type.push(files["avatar"][0].originalname.split(".")[files["avatar"][0].originalname.split(".").length-1])
-		type.push(files["gallery"][0].originalname.split(".")[files["gallery"][0].originalname.split(".").length-1])
-		fs.renameSync(__dirname+"/public/uploads/"+oldname[0],__dirname+"/public/picture/"+oldname[0]+"."+type[0])
-		fs.renameSync(__dirname+"/public/uploads/"+oldname[1],__dirname+"/public/video/"+oldname[1]+"."+type[1])
-		req.files=null
-		req.body.source.picture="/picture/"+oldname[0]+"."+type[0]
-		req.body.source.filepath="/video/"+oldname[1]+"."+type[1]
-	}
-	next()
-})
+
 
 
 var moment=require("moment")
--- config/Middleware.js
+++ config/Middleware.js
@@ -7,7 +7,7 @@ module.exports=function(app){
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({extended:true}))
 	app.use(upload.fields([{ name: "avatar", maxCount: 1 }, { name: "gallery", maxCount: 1 }]))
-	app.use(function(req,res,next){
+	/*app.use(function(req,res,next){
 		if(req.files&&req.files.length){
 			var files=req.files
 			var oldname=[]
@@ -22,6 +22,24 @@ module.exports=function(app){
 			req.files=null
 			req.body.avatar=oldname[0]+"."+type[0]
 			req.body.gallery=oldname[1]+"."+type[1]
+		}
+		next()
+	})*/
+	app.use(function(req,res,next){
+		if(req.files){
+			console.log("youwenjian")
+			var files=req.files
+			var oldname=[]
+			var type=[]
+			oldname.push(files["avatar"][0].filename)
+			oldname.push(files["gallery"][0].filename)
+			type.push(files["avatar"][0].originalname.split(".")[files["avatar"][0].originalname.split(".").length-1])
+			type.push(files["gallery"][0].originalname.split(".")[files["gallery"][0].originalname.split(".").length-1])
+			fs.renameSync(__dirname+"/public/uploads/"+oldname[0],__dirname+"/public/picture/"+oldname[0]+"."+type[0])
+			fs.renameSync(__dirname+"/public/uploads/"+oldname[1],__dirname+"/public/video/"+oldname[1]+"."+type[1])
+			req.files=null
+			req.body.source.picture="/picture/"+oldname[0]+"."+type[0]
+			req.body.source.filepath="/video/"+oldname[1]+"."+type[1]
 		}
 		next()
 	})
